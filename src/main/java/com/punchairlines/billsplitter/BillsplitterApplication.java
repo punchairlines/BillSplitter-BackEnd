760981fbc9d94341f3556c8de47010cd90b3b8fb
YuriyPanchenko <yuriy.panchenko.2000@gmail.com>
2019-07-14 00:15:26 +0300

    push default project

---
+++ src/main/java/com/punchairlines/billsplitter/BillsplitterApplication.java
@@ -0,0 +1,13 @@
+package com.punchairlines.billsplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsplitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillsplitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/punchairlines/billsplitter/BillsplitterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.punchairlines.billsplitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BillsplitterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
